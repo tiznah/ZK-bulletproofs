fda5223fb01897a3a3b53ec7db22f0a70be0e65a
tiz/ <0xtiznah@gmail.com>
2025-11-12 01:06:28 -0700

    Proposed solution

--- chapter-07b.py
+++ chapter-07b.py
@@ -21,7 +21,7 @@ G_vec = [(FQ(6286155310766333871795042970372566906087502116590250812133967451320
      (FQ(15884001095869889564203381122824453959747209506336645297496580404216889561240), FQ(14397810633193722880623034635043699457129665948506123809325193598213289127838)),
      (FQ(6756792584920245352684519836070422133746350830019496743562729072905353421352), FQ(3439606165356845334365677247963536173939840949797525638557303009070611741415))]
 
-H = [(FQ(13728162449721098615672844430261112538072166300311022796820929618959450231493), FQ(12153831869428634344429877091952509453770659237731690203490954547715195222919)),
+H_vec = [(FQ(13728162449721098615672844430261112538072166300311022796820929618959450231493), FQ(12153831869428634344429877091952509453770659237731690203490954547715195222919)),
     (FQ(17471368056527239558513938898018115153923978020864896155502359766132274520000), FQ(4119036649831316606545646423655922855925839689145200049841234351186746829602)),
     (FQ(8730867317615040501447514540731627986093652356953339319572790273814347116534), FQ(14893717982647482203420298569283769907955720318948910457352917488298566832491)),
     (FQ(419294495583131907906527833396935901898733653748716080944177732964425683442), FQ(14467906227467164575975695599962977164932514254303603096093942297417329342836))]
@@ -29,32 +29,52 @@ H = [(FQ(13728162449721098615672844430261112538072166300311022796820929618959450
 Q = (FQ(11573005146564785208103371178835230411907837176583832948426162169859927052980), FQ(895714868375763218941449355207566659176623507506487912740163487331762446439))
 
 
-# return a folded vector of length n/2 for scalars
 def fold(scalar_vec, u):
-    pass
 
-# return a folded vector of length n/2 for points
+    u_inv = pow(u, -1, p)
+    
+    a_prime = []
+    for i in range(0, len(scalar_vec), 2):
+        folded_value = (scalar_vec[i] * u + scalar_vec[i+1] * u_inv) % p
+        a_prime.append(folded_value)
+    
+    return a_prime
+
 def fold_points(point_vec, u):
-    pass
+    u_inv = pow(u, -1, p)
+    
+    G_prime = []
+    for i in range(0, len(point_vec), 2):
+        folded_point = add(multiply(point_vec[i], u), multiply(point_vec[i+1], u_inv))
+        G_prime.append(folded_point)
+    
+    return G_prime
 
-# return (L, R)
 def compute_secondary_diagonal(G_vec, a):
-    pass
+    # L uses even indices of a with odd indices of G
+    # R uses odd indices of a with even indices of G
+    
+    L = add_points(*[multiply(G_vec[i], a[i-1]) for i in range(1, len(a), 2)])
+
+    R = add_points(*[multiply(G_vec[i], a[i+1]) for i in range(0, len(a)-1, 2)])
+    
+    return L, R
+
 
 a = [4,2,42,420]
 b = [2,3,5,8]
 
 P = add_points(vector_commit(G_vec, a), vector_commit(H_vec, b), multiply(Q, np.inner(a, b)))
 
-L1, R1 = # TODO
+L1, R1 = compute_secondary_diagonal(G_vec, a)
 u1 = random_element()
-aprime = # TODO
-Gprime = # TODO
+aprime = fold(a, u1)
+Gprime = fold_points(G_vec, pow(u1, -1, p))
 
 L2, R2 = compute_secondary_diagonal(Gprime, aprime)
 u2 = random_element()
-aprimeprime = # TODO
-Gprimeprime = # TODO
+aprimeprime = fold(aprime, u2)
+Gprimeprime = fold_points(Gprime, pow(u2, -1, p))
 
 assert len(Gprimeprime) == 1 and len(aprimeprime) == 1, "final vector must be len 1"
-assert # TODO -- P ?= aG + bH + abQ
+assert eq(P, add_points(vector_commit(G_vec, a), vector_commit(H_vec, b), multiply(Q, int(np.inner(a, b)) % p))), "invalid proof"
